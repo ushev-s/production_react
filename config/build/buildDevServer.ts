0037ad22beb23bdf0ff7dafe813192bd488d5d02
Sviataslau <ushev.s90@gmail.com>
2022-09-22 22:47:57 +0200

    added ReactRefreshWebpackPlugin

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buildDevServer(options: BuildOptions) {
     port: options.port,
     open: true,
     historyApiFallback: true,
+    hot: true,
   };
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
@@ -19,5 +20,7 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin(),
   ];
 }
