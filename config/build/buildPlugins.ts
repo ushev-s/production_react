6211487e51abb512c57e12e364d6cfcb8ef41e23
Sviataslau <ushev.s90@gmail.com>
2022-10-11 23:40:09 +0200

    fixed dev webpack plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -20,9 +20,6 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ];
 
