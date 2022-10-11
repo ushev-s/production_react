419f542ec45b1f0423f6ef16fa0f228beed2115f
Sviataslau <ushev.s90@gmail.com>
2022-10-11 23:49:08 +0200

    fixed toggle button in sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
@@ -11,7 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className = '' }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
-
+  const { t } = useTranslation();
   const onToggle = () => {
     setCollapsed((prev) => !prev);
   };
@@ -21,9 +22,9 @@ export const Sidebar = ({ className = '' }: SidebarProps) => {
       data-testid="sidebar"
       className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
     >
-      {/* <Button data-testid="sidebar-toggle" onClick={onToggle}>
-        toggle
-      </Button> */}
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        {t('Switch')}
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
