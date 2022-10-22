adeb98161ac20be3e6adb90ec1768894b20c78e6
Sviataslau <ushev.s90@gmail.com>
2022-10-22 16:10:27 +0200

    test

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,6 +1,6 @@
 import { Story } from '@storybook/react';
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
-
+// test
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
   return (
     <ThemeProvider initialTheme={theme}>
